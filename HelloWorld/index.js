a12b15582a457f19b40e7631d40027cbe6e01421
Alina Garea <alinagareasap@gmail.com>
2017-07-24 11:40:30 +0300

    Final version first day

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -4,12 +4,19 @@ var mHttp = require("http");
 //variables
 var iPort = 8081;
 var sMessage = "Hello world";
+var oResponse = {
+
+"returnCode" : "1",
+"returnMessage": "everything OK",
+"returnData": null
+
+};
 //create Http Server
 var oServer = mHttp.createServer(function(req, res){
 console.log("\r\nnoServer created.");
 
 res.writeHead(200, {'Content-Type': 'text/plain'});
-res.write(sMessage);
+res.write(JSON.stringify(oResponse));
 res.end();
 
 console.log("Response sent!");
