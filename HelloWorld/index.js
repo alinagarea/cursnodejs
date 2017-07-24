e0b04f40c5d9f10409f8a3a8c0ec8c53067e6190
Alina Garea <alinagareasap@gmail.com>
2017-07-24 11:22:08 +0300

    Server code update

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -1,2 +1,26 @@
-/* Hello World! program in Node.js */
-console.log("Hello World!");
+"use strict";
+//required module
+var mHttp = require("http");
+//variables
+var iPort = 8081;
+var sMessage = "Hello world";
+//create Http Server
+var oServer = mHttp.createServer(function(req, res){
+console.log("\r\nnoServer created.");
+
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.write(sMessage);
+res.end();
+
+console.log("Response sent!");
+
+});
+
+oServer.listen(iPort);
+
+console.log('Server running at http://127.0.0.1:' + iPort + '/');
+
+//var sMessage = "Hello world";
+//sMessage = sMessage + "!";
+
+console.log(sMessage);
