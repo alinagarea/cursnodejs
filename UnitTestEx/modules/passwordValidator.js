feb9a67fecc278e00d35be7e7b9ad1d71b7bb89d
Alina Garea <alinagareasap@gmail.com>
2017-07-25 12:31:46 +0300

    Test implementat

--- UnitTestEx/modules/passwordValidator.js
+++ UnitTestEx/modules/passwordValidator.js
@@ -1,4 +1,16 @@
 exports.validPassword = module.exports.validPassword = function(password){
 
+    if(password.length > 8){
+        return false;
+
+    }
+    
+    if(password.indexOf('testegratis') >= 0){
+        return false;
+
+    }
+
     return true;
+
+
 }
