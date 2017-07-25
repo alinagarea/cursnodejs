877df246bfeccd64e7ca8f431040898a0ab1e377
Alina Garea <alinagareasap@gmail.com>
2017-07-25 10:23:38 +0300

    Exit and BeforeExit

---
+++ GObjEx/index.js
@@ -0,0 +1,22 @@
+process.on('beforeExit', function(code) {
+
+    // Following code will never execute.
+//    setTimeout(function() {
+//       console.log("This will not run");
+//    }, 0);
+  
+   console.log('About to exit with code:', code);
+});
+
+
+// process.on('exit', function(code) {
+
+    
+   // Following code will never execute.
+//    setTimeout(function() {
+//       console.log("This will not run");
+//    }, 0);
+  
+//    console.log('About to exit with code:', code);
+// });
+console.log("Program Ended");
